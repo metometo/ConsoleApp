ad4fe467281c0ddce5eb0fc3ad18a19362af27d4
Metodi Dimitrov <metodi.dimitrov@yahoo.com>
2020-03-28 14:25:08 +0200

    рере

--- GitConsoleApplication/Program.cs
+++ GitConsoleApplication/Program.cs
@@ -11,6 +11,7 @@ namespace GitConsoleApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Здравей");
+            Console.WriteLine("Отново");
         }
     }
 }
