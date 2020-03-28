6c9dfb6e64ee713303c62f52e2b0c959cf58b85c
Metodi Dimitrov <metodi.dimitrov@yahoo.com>
2020-03-28 14:27:13 +0200

    ds

---
+++ GitConsoleApplication/Class2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitConsoleApplication
+{
+    class Class2
+    {
+        int id;
+        int id2;
+    }
+}
